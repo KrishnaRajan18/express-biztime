b429f720c05d458b5218cc5d0da56db5737f1346
krishnarajan <krishnarajan018@gmail.com>
2021-03-09 21:37:14 -0800

    second exercise

--- routes/companies.js
+++ routes/companies.js
@@ -25,7 +25,40 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-
+router.get("/:code", async function (req, res, next) {
+    try {
+      let code = req.params.code;
+  
+      const compResult = await db.query(
+            `SELECT code, name, description
+             FROM companies
+             WHERE code = $1`,
+          [code]
+      );
+  
+      const invResult = await db.query(
+            `SELECT id
+             FROM invoices
+             WHERE comp_code = $1`,
+          [code]
+      );
+  
+      if (compResult.rows.length === 0) {
+        throw new ExpressError(`No such company: ${code}`, 404)
+      }
+  
+      const company = compResult.rows[0];
+      const invoices = invResult.rows;
+  
+      company.invoices = invoices.map(inv => inv.id);
+  
+      return res.json({"company": company});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+  });
 
 router.post("/", async function (req, res, next) {
   try {
